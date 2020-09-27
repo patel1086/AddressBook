0f1c039707c6db3b33f19c402c91d478331c5394
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-09-27 18:54:51 +0530

    First Commit

---
+++ Address.java
@@ -0,0 +1,29 @@
+import java.util.*; //importing java package
+public class Address {
+	
+	public String firstname;
+	public String lastname;
+	public String address;
+	public String city;
+	public String state;
+	public String zip;
+	public String number;
+	public String email;
+	
+	//constructor
+	public Address(String firstname, String lastname, String address, String city, String state, String zip, String number,
+			String email) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.number = number;
+		this.email = email;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address book");
+	}
+}
